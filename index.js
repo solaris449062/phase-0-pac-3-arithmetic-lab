b790f8cbfd8a3368fee598a4995888a813523150
Sungoh Park <spark449062@gmail.com>
2021-06-27 16:06:31 -0600

    Completed assignment

---
+++ index.js
@@ -0,0 +1,31 @@
+function makeInt(inputString) {
+    return parseInt(inputString, 10);
+}
+
+function preserveDecimal(inputString) {
+    return parseFloat(inputString);
+}
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function increment(n) {
+    return ++n;
+}
+
+function decrement(n) {
+    return --n;
+}
